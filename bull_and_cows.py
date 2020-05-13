a7aca1346892dd2a98d0130b575e2cc0cbe85722
Dominik <domzurek@student.agh.edu.pl>
2020-05-13 15:43:37 +0200

    Update bull_and_cows.py
    Przerobiłem cały kod żeby był napisany funkcyjnie. Dodałem możliwość wyboru długości kodu oraz poziomu trudności gry. Poprawiłem również wyświetlanie się wartości cows(które mają wskazywać na poprawny element kodu, ale w nie tym miejscu co trzeba, ale możliwe, że jeszcze trzeba będzie to poprawić drugi raz)

--- bull_and_cows.py
+++ bull_and_cows.py
@@ -1,29 +1,79 @@
 import random
 
-# TODO: dodaj opcję wyboru długości odgadywanego kodu
-ran_num=[]
-for x in range(4):
-    ran_num.append(str(random.randint(0,9)))
-# TODO: stwórz poprawne wpisywanie wejścia, nie rzutuj chamsko na stringa na listę
-user_num=list((input("Podaj 4 cyfrową liczbę: ")))
-main_counter=1
+
+def main():
+    def get_code_lenght():
+        available_range = [str(x) for x in range(3, 11)]
+        code_lenght = input("Podaj dlugosc zgadywanego kodu(z zakresu od 3 do 10 znaków): ")
+        while code_lenght not in available_range:
+            print("Nieporawna dlugosc kodu!")
+            code_lenght = input("Podaj dlugosc zgadywanego kodu(z zakresu od 3 do 10 znaków): ")
+        return int(code_lenght)
+
+    def generate_code(code_lenght):
+        return [str(random.randint(0, 9)) for x in range(code_lenght)]
+
+    def get_user_code(code_lenght):
+        """ TODO: stwórz poprawne wpisywanie wejścia, nie rzutuj chamsko na stringa na listę
+            Dominik edit: nie mam pomysłu jak nie rzutować po chamsku stringa na listę, ale dodałem kontrolę poprawności
+            wprowadzanych danych przez użytkownika oraz chyba w poprzedniej wersji program podawał niepoprawnie wartości
+            cows, więc to zmieniłem
+        """
+        retry = True
+        available_chars = [str(x) for x in range(0, 10)]
+        user_input = []
+        while retry:
+            retry = False
+            user_input = list(input("Podaj {}-cyfrowy kod: ".format(code_lenght)))
+            if len(user_input) != code_lenght:
+                retry = True
+            else:
+                for i in user_input:
+                    if i not in available_chars:
+                        retry = True
+        return user_input
+
+    def set_difficulty_lvl(code_lenght):
+        available_diff_lvls={"easy":4*code_lenght,"medium":3*code_lenght,"hard":2*code_lenght,"impossible":code_lenght}
+        print("Wybierz poziom trudnosci gry:")
+        for i in available_diff_lvls.keys():
+            print("{} - {} prób".format(i,available_diff_lvls[i]))
+        diff = input("Wskaż poziom trudności na którym chcesz grać: ")
+        while diff not in available_diff_lvls.keys():
+            print("Niepoprawny poziom trudności!")
+            diff = input("Wskaż poziom trudności na którym chcesz grać: ")
+        return available_diff_lvls[diff]
+
+    def gameplay(ran_num, user_num, code_lenght, difficulty):
+        main_counter = 1
+        while ran_num != user_num:
+            main_counter += 1
+            bulls = 0
+            cows = 0
+            # check for cows
+            for i, num in enumerate(user_num):
+                if num in ran_num:
+                    cows += 1
+            # check for bulls
+            for num_i in range(code_lenght):
+                if ran_num[num_i] == user_num[num_i]:
+                    bulls += 1
+            cows -= bulls
+            print(cows, " cows ", bulls, " bulls")
+            if(main_counter <= difficulty):
+                print("Spróbuj zgadnąć jeszcze raz. Zostało Ci {} prób!".format(1+difficulty-main_counter))
+                user_num = get_user_code(code_lenght)
+            else:
+                print("Niestety przekroczyłeś liczbę możliwych prób :(\nPoprawnym kodem było {}".format(ran_num))
+                break
+        else:
+            print("Gratulacje! Zgadłeś za " + str(main_counter) + " razem!")
+
+    code_lenght = get_code_lenght()
+    ran_num = generate_code(code_lenght)
+    difficulty = set_difficulty_lvl(code_lenght)
+    user_num = get_user_code(code_lenght)
+    gameplay(ran_num, user_num, code_lenght, difficulty)
 
 
-while ran_num != user_num :
-    main_counter+=1
-    bulls=0
-    cows=0
-    #check for cows
-    for i,num in enumerate(user_num):
-        if num in ran_num and (i-1)!=ran_num.index(num):
-            cows+=1
-            print("gotten cow!")
-    #check for bulls
-    for num_i in  range(4):
-        if ran_num[num_i] == user_num[num_i]:
-            bulls+=1
-
-    print(cows," cows ", bulls, " bulls")
-    user_num=list((input("Spróbuj zgadnąć jeszcze raz: ")))
-
-print("Gratulacje! Zgadłeś za " + str(main_counter) + " razem!")
+main()
