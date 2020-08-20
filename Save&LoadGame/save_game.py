da778fe9a6560e329284424211887c7c7c2aaa2f
justynagibas <63740388+justynagibas@users.noreply.github.com>
2020-08-20 12:00:32 +0200

    Update save_game.py
    Drobne zmiany w kodzie

--- Save&LoadGame/save_game.py
+++ Save&LoadGame/save_game.py
@@ -6,23 +6,21 @@ import json
 # nalezy utworzyc slownik my_dict, w ktorym beda znajdowaly sie informacje o kolorach kazdego z przyciskow na planszy
 class SaveData:
     def __init__(self,):
-        # w self.colors chce przechowywac slownik, w ktorym beda informacje o kolorach kazdego przycisku
-        self.colors = {}
+        # w self.colors chce przechowywac tabele, o stanie planszy gry
+        self.colors = []
         self.level = None
         self.active_row = 0
         self.n_pegs = 0
 
-    def save_game(self,my_dict,a_row,lev,n_pegs_):
-        with open('save.json', 'w') as outfile:
-            json.dump(my_dict, outfile)
-            self.colors = my_dict
-            outfile.close()
+    def save_game(self,state_tab,a_row,lev,n_pegs_):
         self.active_row = a_row
         self.n_pegs = n_pegs_
         self.level = lev
-
+        self.colors = state_tab
+        with open('save.json', 'w') as outfile:
+            json.dump(self.__dict__, outfile)
+          
     def load_game(self):
         with open('save.json','r') as file:
-            self.colors = json.load(file)
-            file.close()
+            self.__dict__ = json.load(file)
         # tutaj nalezy wywolac plansze z odpowiednimi danymi przechowywanymi w tej klasie
