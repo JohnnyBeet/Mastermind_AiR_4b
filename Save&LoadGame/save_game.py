4936daf09eeebdd5e0de4aa92e50814276647660
justynagibas <63740388+justynagibas@users.noreply.github.com>
2020-09-03 13:59:35 +0200

    Update save_game.py
    Zmiana zapisywanych parametrów.

--- Save&LoadGame/save_game.py
+++ Save&LoadGame/save_game.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 import json
 
 
@@ -8,14 +8,12 @@ class SaveData:
     def __init__(self,):
         # w self.colors chce przechowywac tabele, o stanie planszy gry
         self.colors = []
-        self.level = None
         self.active_row = 0
         self.n_pegs = 0
 
-    def save_game(self,state_tab,a_row,lev,n_pegs_):
+    def save_game(self,state_tab,a_row,,n_pegs_):
         self.active_row = a_row
         self.n_pegs = n_pegs_
-        self.level = lev
         self.colors = state_tab
         with open('Save&LoadGame/save.txt', 'w') as outfile:
             json.dump(self.__dict__, outfile)
@@ -23,4 +21,4 @@ class SaveData:
     def load_game(self):
         with open('Save&LoadGame/save.txt', 'r') as file:
             self.__dict__ = json.load(file)
-        # tutaj nalezy wywolac plansze z odpowiednimi danymi przechowywanymi w tej klasie
+        
