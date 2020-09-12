52258765bc43f16ed958157846eb97409fd3c5bf
qliasz <KEliasz145@gmail.com>
2020-09-12 18:12:16 +0200

    Revert "Merge branch 'master' of https://github.com/JohnnyBeet/Mastermind_AiR_4b"
    This reverts commit e2f07c392702e36637ea68bc3acfe429e8ec6326, reversing
    changes made to 0c5dbb399e01f63e0dc0931428f431854a8645e9.

--- SaveAndLoadGame/save_game.py
+++ SaveAndLoadGame/save_game.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 import json
-from src.game_classes import *
+import pygame
 screen = pygame.display.set_mode((400, 400))
 
 
-# nalezy utworzyc slownik my_dict, w ktorym beda znajdowaly sie informacje o kolorach kazdego z przyciskow na planszy
+#nalezy utworzyc slownik my_dict, w ktorym beda znajdowaly sie informacje o kolorach kazdego z przyciskow na planszy
 class SaveData:
     def __init__(self):
         self.active_row = 0
         self.n_pegs = 0
         self.rows = 0
         self.game_type = None
-        self.rows_of_pegs = None
+        # self.rows_of_pegs = None
         self.winning_code = []
 
     def save_game(self, a_row, n_pegs_, rows, game_type, _rows_of_pegs, _winning_code):
@@ -21,19 +21,19 @@ class SaveData:
             for i, row in enumerate(_rows_of_pegs):
                 for peg in row:
                     json_data.append(peg.to_json())
-            with open('SavedAndLoad/json_data', 'w') as f:
+            with open('SaveAndLoadGame/json_data.txt', 'w+') as f:
                 json.dump(json_data, f)
         elif game_type == 'Letter':
             for i, row in enumerate(_rows_of_pegs):
                 for letter in row:
                     json_data.append(letter.to_json())
-            with open('SavedAndLoad/json_data', 'w') as f:
+            with open('SaveAndLoadGame/json_data.txt', 'w+') as f:
                 json.dump(json_data, f)
         self.active_row = a_row
         self.n_pegs = n_pegs_
         self.rows = rows
         self.game_type = game_type
-        self.rows_of_pegs = _rows_of_pegs
+        # self.rows_of_pegs = _rows_of_pegs
         self.winning_code = _winning_code
         with open('SaveAndLoadGame/save.txt', 'w') as outfile:
             json.dump(self.__dict__, outfile)
@@ -47,7 +47,7 @@ class SaveData:
         with open('SaveAndLoadGame/save.txt', 'r') as file:
             self.__dict__ = json.load(file)
 
-        with open('SaveAndLoad/json_data', 'r') as n:
+        with open('SaveAndLoadGame/json_data', 'r') as n:
             json_data = json.load(n)
 
         if self.game_type == 'Peg':
